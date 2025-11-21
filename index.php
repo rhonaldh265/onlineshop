be2a1278a5cb6d71d5529fa1b708baaf0933edc3
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 17:55:09 +0300

    Remove JSON serving logic from index.php
    Removed JSON data serving logic and retained redirection to UI.

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-// If explicitly requesting JSON (like from fetch), serve data
-if (isset($_GET['json'])) {
-    header('Content-Type: application/json');
-    $dataFile = __DIR__ . "/uploads/products.json";
-    $products = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
-    echo json_encode($products);
-    exit;
-}
-
-// Otherwise, redirect to UI
 header("Location: index.html");
 exit;
 ?>
