6b0dd6ac37ed74213ba40c1c3bf341fd8903be5f
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 17:05:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+header('Content-Type: application/json');
+
 $dataFile = __DIR__ . "/uploads/products.json";
 $products = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
+
+echo json_encode($products);
 ?>
 
-<!-- Load the UI from index.html -->
-<?php include("index.html"); ?>
