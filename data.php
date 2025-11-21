43f670884d7ab38b638c6e06930ddedbbd9932cc
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 17:45:48 +0300

    Create data.php to serve JSON products data

---
+++ data.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: application/json');
+$dataFile = __DIR__ . "/uploads/products.json";
+$products = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
+echo json_encode($products);
+?>
