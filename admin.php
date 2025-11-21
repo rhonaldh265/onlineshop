4b8d3dea41df54947673cf9eb61a0cd399724b03
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 15:45:30 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,6 +4,14 @@
   <title>Admin Panel</title>
 </head>
 <body>
+
+<div style="position: absolute; top: 10px; right: 10px;">
+  <form action="admin-login.php" method="post">
+    <input type="password" name="pin" placeholder="Enter Admin PIN">
+    <input type="submit" value="Admin Panel">
+  </form>
+</div>
+
   <h1>Add New Product</h1>
   <form action="upload.php" method="post" enctype="multipart/form-data">
     <label>Product Name:</label><input type="text" name="name"><br>
