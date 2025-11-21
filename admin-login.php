112b0f4c5f14134a6f4287f0d35e07c9abeae210
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 15:54:38 +0300

    Refactor admin login to use HTML form
    Replaced PIN validation logic with a simple HTML form for admin login.

--- admin-login.php
+++ admin-login.php
@@ -1,10 +1,13 @@
-<?php
-$correctPin = "rootAdmin77";
-
-if ($_POST["pin"] === $correctPin) {
-    header("Location: admin.php");
-    exit();
-} else {
-    echo "Incorrect PIN. <a href='index.php'>Go back</a>";
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Login</title>
+</head>
+<body>
+  <h1>Admin Login</h1>
+  <form action="admin-login.php" method="post">
+    <input type="password" name="pin" placeholder="Enter Admin PIN">
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
