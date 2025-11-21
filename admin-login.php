d478e8619dcb9483decbd53934a4e22991f28e16
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 15:56:09 +0300

    Update admin-login.php

--- admin-login.php
+++ admin-login.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Admin Login</title>
-</head>
-<body>
-  <h1>Admin Login</h1>
-  <form action="admin-login.php" method="post">
-    <input type="password" name="pin" placeholder="Enter Admin PIN">
-    <input type="submit" value="Login">
-  </form>
-</body>
-</html>
+<?php
+$correctPin = "rootAdmin77";
+
+if ($_POST["pin"] === $correctPin) {
+    header("Location: admin.php");
+    exit();
+} else {
+    echo "Incorrect PIN. <a href='admin-login.html'>Try again</a>";
+}
+?>
