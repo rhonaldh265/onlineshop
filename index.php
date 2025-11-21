32ef6aff00de35c88a27f7f1c0e2305e28455929
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 15:19:54 +0300

    Add initial product catalog page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$products = json_decode(file_get_contents("products.json"), true);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Classic Shop</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Product Catalog</h1>
+  <div class="product-grid">
+    <?php foreach ($products as $p): ?>
+      <div class="product-card">
+        <div class="product-image">
+          <img src="uploads/<?php echo $p['image']; ?>" alt="<?php echo $p['name']; ?>">
+        </div>
+        <div class="product-info">
+          <h3><?php echo $p['name']; ?></h3>
+          <p>Material: <?php echo $p['material']; ?></p>
+          <p>Size: <?php echo $p['size']; ?></p>
+          <span class="price">KES <?php echo $p['price']; ?></span>
+          <button class="add-btn">Add to Basket</button>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</body>
+</html>
