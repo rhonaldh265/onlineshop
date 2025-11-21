d9db3c343c664b47bf97fb2a6b91bc02def67a5f
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 18:33:54 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,33 +2,33 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$dataFile = __DIR__ . "/uploads/products.json";
 
-// Ensure JSON file exists
-if (!file_exists($dataFile)) {
-    file_put_contents($dataFile, "[]");
+$dataFile = __DIR__ . "/uploads/products.json";
+$products = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
+
+$name = $_POST['name'];
+$material = $_POST['material'];
+$size = $_POST['size'];
+$price = $_POST['price'];
+
+$imageFiles = [];
+foreach ($_FILES['images']['tmp_name'] as $key => $tmp_name) {
+    $filename = uniqid() . "-" . basename($_FILES['images']['name'][$key]); 
+    $target = __DIR__ . "/uploads/" . $filename;
+    if (move_uploaded_file($tmp_name, $target)) {
+        $imageFiles[] = $filename;
+    }
 }
 
-// Load existing products
-$products = json_decode(file_get_contents($dataFile), true);
-
-// Save image
-$targetDir = __DIR__ . "/uploads/";
-$imageName = basename($_FILES["image"]["name"]);
-move_uploaded_file($_FILES["image"]["tmp_name"], $targetDir . $imageName);
-
-// Add new product
+// Add product entry with all uploaded images
 $products[] = [
-    "name" => $_POST["name"],
-    "material" => $_POST["material"],
-    "size" => $_POST["size"],
-    "price" => $_POST["price"],
-    "image" => $imageName
+    "name" => $name,
+    "material" => $material,
+    "size" => $size,
+    "price" => $price,
+    "images" => $imageFiles
 ];
 
-// Save back to JSON
 file_put_contents($dataFile, json_encode($products, JSON_PRETTY_PRINT));
-
-echo "Product uploaded successfully! <a href='index.php'>View Catalog</a>";
+header("Location: index.html");
 ?>
-
