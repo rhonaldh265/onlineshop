1e305110b4d07743447d93fb963c1798ee68e3e3
rhonaldh265 <ronaldkiprotich001@gmail.com>
2025-11-21 17:49:35 +0300

    Update data.php

--- data.php
+++ data.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: application/json');
+
 $dataFile = __DIR__ . "/uploads/products.json";
 $products = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
+
 echo json_encode($products);
 ?>
